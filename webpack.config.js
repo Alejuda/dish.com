1e2e4abfab61ea96c012a50f03f59a4a456258af
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-22 13:46:41 -0300

    Add webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        type: 'asset',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  }
+};
