0eb2731e5dddc53c4765217963173d7933ae70c0
ahmed025566 <ahmedsalah025566@gmail.com>
2023-05-23 02:54:11 +0300

    Render the elemnts

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
-import "./style.css";
-console.log("Hello webpack!");
+import './style.css';
+import logo from './assets/logo.png';
+import { selectedDrop, getMyData } from './modules/renderElements.js';
+
+const submit = document.querySelector('.submit');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
+const catogray = document.getElementById('catogary');
+const typeOrArea = document.getElementById('typeOrArea');
+const renderLogo = () => {
+  const myLogo = new Image();
+  myLogo.src = logo;
+  myLogo.className = 'logo';
+  header.append(myLogo);
+};
+
+const fillFooter = () => {
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  footer.append(document.createTextNode(`Copyright © ${currentYear} Ahmed & Nacho. All Rights Reserved`));
+};
+
+catogray.addEventListener('change', () => {
+  typeOrArea.innerHTML = '';
+  selectedDrop(catogray.value);
+});
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  getMyData();
+});
+window.addEventListener('DOMContentLoaded', () => {
+  renderLogo();
+  fillFooter();
+  selectedDrop('c')
+    .then(() => {
+      getMyData();
+    });
+});
---
+++ src/modules/renderElements.js
@@ -0,0 +1,50 @@
+const catogray = document.getElementById('catogary');
+const typeOrArea = document.getElementById('typeOrArea');
+const displayDiv = document.querySelector('.displayDiv');
+
+export const selectedDrop = async (selected) => {
+  await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?${selected}=list`)
+    .then((response) => response.json())
+    .then((data) => {
+      const { meals } = data;
+
+      meals.forEach((element) => {
+        if (typeOrArea.children.length < 28) {
+          const choice = document.createElement('option');
+          if ((+Object.values(element)[0]) % 1 === 0) {
+            choice.append(document.createTextNode(Object.values(element)[1]));
+            choice.value = Object.values(element)[1];
+          } else {
+            choice.append(document.createTextNode(Object.values(element)[0]));
+            choice.value = Object.values(element)[0];
+          }
+
+          typeOrArea.append(choice);
+        }
+      });
+    });
+};
+
+export const getMyData = async () => {
+  await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?${catogray.value}=${typeOrArea.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const { meals } = data;
+
+      displayDiv.innerHTML = '';
+      meals.forEach((meal) => {
+        const container = document.createElement('div');
+        container.className = 'container';
+        const maelDiv = document.createElement('div');
+        maelDiv.className = 'meal';
+        maelDiv.setAttribute('data-id', meal.idMeal);
+        maelDiv.innerHTML = `<img src="${meal.strMealThumb}" alt="Meal Photo" class = "mealPhoto">`;
+        const mealName = document.createElement('p');
+        mealName.className = 'mealName';
+        mealName.append(document.createTextNode(meal.strMeal));
+        container.append(maelDiv);
+        container.append(mealName);
+        displayDiv.append(container);
+      });
+    });
+};
