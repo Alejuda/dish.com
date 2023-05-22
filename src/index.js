2def3f6dc65b278b78f5b8725c7f438eccbc1666
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-22 17:50:40 -0300

    Add close popup function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./style.css";
 import showPopup from "./modules/showPopup.js";
+import closePopup from "./modules/closePopup";
 import closeImg from './close.svg'
 
-const closePopup = document.getElementById('close-btn');
-closePopup.src = closeImg;
+const closePopupImg = document.getElementById('close-btn');
+closePopupImg.src = closeImg;
+closePopupImg.addEventListener('click', () => closePopup());
+
 window.showPopup = showPopup;
