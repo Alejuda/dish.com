3884cc2d94218f79f63e32c27ab068be0e0d8f4c
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-23 15:16:44 -0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import "./style.css";
-import logo from "./assets/logo.png";
-import { selectedDrop, getMyData } from "./modules/renderElements.js";
-import showPopup from "./modules/showPopup.js";
-import closePopup from "./modules/closePopup.js";
-import closeImg from "./close.svg";
-import postCommit from "./modules/postCommit.js";
-import fetchCommmits from "./modules/fetchCommits.js";
+import './style.css';
+import logo from './assets/logo.png';
+import { selectedDrop, getMyData } from './modules/renderElements.js';
+import showPopup from './modules/showPopup.js';
+import closePopup from './modules/closePopup.js';
+import closeImg from './close.svg';
+import postCommit from './modules/postCommit.js';
+import fetchCommmits from './modules/fetchCommits.js';
 
-const submit = document.querySelector(".submit");
-const header = document.querySelector(".header");
-const footer = document.querySelector(".footer");
-const catogray = document.getElementById("catogary");
-const typeOrArea = document.getElementById("typeOrArea");
-const closePopupImg = document.getElementById("close-btn");
+const submit = document.querySelector('.submit');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
+const catogray = document.getElementById('catogary');
+const typeOrArea = document.getElementById('typeOrArea');
+const closePopupImg = document.getElementById('close-btn');
 
 const renderLogo = () => {
   const myLogo = new Image();
   myLogo.src = logo;
-  myLogo.className = "logo";
+  myLogo.className = 'logo';
   header.append(myLogo);
 };
 
@@ -26,45 +26,44 @@ const fillFooter = () => {
   const currentYear = today.getFullYear();
   footer.append(
     document.createTextNode(
-      `Copyright © ${currentYear} Ahmed & Nacho. All Rights Reserved`
-    )
+      `Copyright © ${currentYear} Ahmed & Nacho. All Rights Reserved`,
+    ),
   );
 };
 
-catogray.addEventListener("change", () => {
-  typeOrArea.innerHTML = "";
+catogray.addEventListener('change', () => {
+  typeOrArea.innerHTML = '';
   selectedDrop(catogray.value);
 });
 
-submit.addEventListener("click", (e) => {
+submit.addEventListener('click', (e) => {
   e.preventDefault();
   getMyData();
 });
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   renderLogo();
   fillFooter();
-  selectedDrop("c").then(() => {
+  selectedDrop('c').then(() => {
     getMyData();
   });
 });
 
 closePopupImg.src = closeImg;
-closePopupImg.addEventListener("click", () => closePopup());
+closePopupImg.addEventListener('click', () => closePopup());
 
 window.showPopup = showPopup;
 
-const postForm = document.getElementById("post-comment-form");
-const nameInput = document.getElementById("name");
-const commentInput = document.getElementById("commit");
-const commitContainer = document.getElementById("commits-container");
+const postForm = document.getElementById('post-comment-form');
+const nameInput = document.getElementById('name');
+const commentInput = document.getElementById('commit');
+const commitContainer = document.getElementById('commits-container');
 
-postForm.addEventListener("submit", async (e) => {
+postForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   await postCommit(window.popupElementd, nameInput.value, commentInput.value);
   const newCommits = await fetchCommmits(window.popupElementd);
-  console.log(newCommits);
   commitContainer.innerHTML = newCommits;
   nameInput.value = '';
-  commentInput.value= '';
+  commentInput.value = '';
 });
--- src/modules/postCommit.js
+++ src/modules/postCommit.js
@@ -4,16 +4,14 @@ const addCommit = async (mealId, user, comment) => {
     username: `${user}`,
     comment: `${comment}`,
   };
-  const apiUrl =
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/o5YLWfucVuUSY3jbCcrI/comments";
-  const response = await fetch(apiUrl, {
-    method: "POST",
+  const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/o5YLWfucVuUSY3jbCcrI/comments';
+  await fetch(apiUrl, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(postBody),
   });
-  console.log(response);
 };
 
 export default addCommit;
